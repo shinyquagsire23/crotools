4d4582604c2f70307489d56e3ecbf0f39a3073a4
shinyquagsire23 <mtinc2@gmail.com>
2017-12-14 16:39:22 -0700

    elf2cro: Create CRO_Context, fixes UAFs in cro_header

--- elf2cro/elf2cro.cpp
+++ elf2cro/elf2cro.cpp
@@ -7,27 +7,36 @@
 
 using namespace ELFIO;
 
-void push_data(void*& cro_data, size_t& cro_size, const void* data, size_t size)
+typedef struct
 {
-   cro_data = realloc(cro_data, cro_size + size);
+   void* cro_data;
+   size_t cro_size;
+   CRO_Header* cro_header;
+} CRO_Context;
+
+void push_data(CRO_Context& context, const void* data, size_t size)
+{
+   context.cro_data = realloc(context.cro_data, context.cro_size + size);
    if (data != NULL)
-      memcpy((char*)cro_data + cro_size, data, size);
+      memcpy((char*)context.cro_data + context.cro_size, data, size);
    else
-      memset((char*)cro_data + cro_size, 0, size);
-   cro_size += size;
+      memset((char*)context.cro_data + context.cro_size, 0, size);
+   context.cro_size += size;
+   context.cro_header = (CRO_Header*)context.cro_data;
 }
 
-void push_segment(void* cro_data, size_t& cro_size, segment* seg)
+void push_segment(CRO_Context& context, segment* seg)
 {
-   push_data(cro_data, cro_size, seg->get_data(), seg->get_file_size());
+   push_data(context, seg->get_data(), seg->get_file_size());
 }
 
-void alloc_align_up(void*& alloc, size_t& size, size_t align, uint8_t fill = 0)
+void cro_align_up(CRO_Context& context, size_t align, uint8_t fill = 0)
 {
-   size_t old_size = size;
-   size = (size + (align - size % align) % align);
-   alloc = realloc(alloc, size);
-   memset((char*)alloc + old_size, fill, size - old_size);
+   size_t old_size = context.cro_size;
+   context.cro_size = (context.cro_size + (align - context.cro_size % align) % align);
+   context.cro_data = realloc(context.cro_data, context.cro_size);
+   context.cro_header = (CRO_Header*)context.cro_data;
+   memset((char*)context.cro_data + old_size, fill, context.cro_size - old_size);
 }
 
 int main(int argc, char **argv)
@@ -46,31 +55,33 @@ int main(int argc, char **argv)
       return -1;
    }
    
-   CRO_Header *cro_header;
-   void* cro_data = calloc(1, sizeof(CRO_Header));
-   size_t cro_size = sizeof(CRO_Header);
+   CRO_Context cro_ctx;
+   cro_ctx.cro_data = calloc(1, sizeof(CRO_Header));
+   cro_ctx.cro_size = sizeof(CRO_Header);
+   cro_ctx.cro_header = (CRO_Header*)cro_ctx.cro_data;
+   
+   
    size_t segment_start[5];
    size_t segment_size[5];
    
-   alloc_align_up(cro_data, cro_size, 0x80);
-   size_t cro_header_size = cro_size;
+   cro_align_up(cro_ctx, 0x80);
+   size_t cro_header_size = cro_ctx.cro_size;
 
-   cro_header = (CRO_Header*)cro_data;
-   cro_header->magic = MAGIC_CRO0;
+   cro_ctx.cro_header->magic = MAGIC_CRO0;
    
    // Push .text segment
-   segment_start[SEG_TEXT] = cro_size;
-   push_segment(cro_data, cro_size, elf.segments[SEG_TEXT]);
+   segment_start[SEG_TEXT] = cro_ctx.cro_size;
+   push_segment(cro_ctx, elf.segments[SEG_TEXT]);
    
    // Push .rodata segment
-   segment_start[SEG_RODATA] = cro_size;
-   segment_size[SEG_TEXT] = cro_size - segment_start[SEG_TEXT];
-   push_segment(cro_data, cro_size, elf.segments[SEG_RODATA]);
-   alloc_align_up(cro_data, cro_size, 0x1000);
-   segment_size[SEG_RODATA] = cro_size - segment_start[SEG_RODATA];
+   segment_start[SEG_RODATA] = cro_ctx.cro_size;
+   segment_size[SEG_TEXT] = cro_ctx.cro_size - segment_start[SEG_TEXT];
+   push_segment(cro_ctx, elf.segments[SEG_RODATA]);
+   cro_align_up(cro_ctx, 0x1000);
+   segment_size[SEG_RODATA] = cro_ctx.cro_size - segment_start[SEG_RODATA];
    
    // Set .bss size
-   cro_header->size_bss = elf.segments[SEG_BSS]->get_memory_size();
+   cro_ctx.cro_header->size_bss = elf.segments[SEG_BSS]->get_memory_size();
    
    //
    // Begin linking info
@@ -80,16 +91,16 @@ int main(int argc, char **argv)
    std::string cro_filename = std::string(argv[2]);
    std::string cro_name = cro_filename.substr(0, cro_filename.find_last_of("."));
    
-   cro_header->offs_mod_name = cro_size;
-   cro_header->offs_name = cro_size;
-   cro_header->size_name = cro_name.size() + 1;
+   cro_ctx.cro_header->offs_mod_name = cro_ctx.cro_size;
+   cro_ctx.cro_header->offs_name = cro_ctx.cro_size;
+   cro_ctx.cro_header->size_name = cro_name.size() + 1;
    
-   push_data(cro_data, cro_size, cro_name.c_str(), cro_name.size() + 1);
+   push_data(cro_ctx, cro_name.c_str(), cro_name.size() + 1);
 
    
    // Push segments
-   alloc_align_up(cro_data, cro_size, 0x4);
-   size_t cro_segment_start = cro_size;
+   cro_align_up(cro_ctx, 0x4);
+   size_t cro_segment_start = cro_ctx.cro_size;
    for (int i = 0; i < 5; i++)
    {
       CRO_Segment segment;
@@ -105,27 +116,27 @@ int main(int argc, char **argv)
          segment.type = SEG_TEXT;
       }
       
-      push_data(cro_data, cro_size, &segment, sizeof(CRO_Segment));
+      push_data(cro_ctx, &segment, sizeof(CRO_Segment));
    }
-   cro_header->offs_segments = cro_segment_start;
-   cro_header->num_segments = 5;
+   cro_ctx.cro_header->offs_segments = cro_segment_start;
+   cro_ctx.cro_header->num_segments = 5;
    
    // Push exported symbols
    symbol_section_accessor syma(elf, elf.sections[".dynsym"]);
-   CRO_Symbol* exports = (CRO_Symbol*)((char*)cro_data + cro_size);
-   cro_header->offs_symbol_exports = cro_size;
-   cro_header->offs_index_exports = syma.get_symbols_num();
-   push_data(cro_data, cro_size, NULL, sizeof(CRO_Symbol) * syma.get_symbols_num());
+   CRO_Symbol* exports = (CRO_Symbol*)((char*)cro_ctx.cro_data + cro_ctx.cro_size);
+   cro_ctx.cro_header->offs_symbol_exports = cro_ctx.cro_size;
+   cro_ctx.cro_header->offs_index_exports = syma.get_symbols_num();
+   push_data(cro_ctx, NULL, sizeof(CRO_Symbol) * syma.get_symbols_num());
    
    // Push index exports (TODO)
-   cro_header->offs_index_exports = cro_size;
-   cro_header->num_index_exports = syma.get_symbols_num();
+   cro_ctx.cro_header->offs_index_exports = cro_ctx.cro_size;
+   cro_ctx.cro_header->num_index_exports = syma.get_symbols_num();
    
    // Push export tree
-   CRO_ExportTreeEntry* exportTree = (CRO_ExportTreeEntry*)((char*)cro_data + cro_size);
-   push_data(cro_data, cro_size, NULL, sizeof(CRO_ExportTreeEntry) * syma.get_symbols_num());
+   CRO_ExportTreeEntry* exportTree = (CRO_ExportTreeEntry*)((char*)cro_ctx.cro_data + cro_ctx.cro_size);
+   push_data(cro_ctx, NULL, sizeof(CRO_ExportTreeEntry) * syma.get_symbols_num());
    
-   size_t export_strtab_offset = cro_size;
+   size_t export_strtab_offset = cro_ctx.cro_size;
    size_t export_strtab_size;
    for (int i = 0; i < syma.get_symbols_num(); i++)
    {
@@ -138,9 +149,9 @@ int main(int argc, char **argv)
       
       if (section_index != 0 && name != "")
       {
-         size_t name_offset = cro_size;
+         size_t name_offset = cro_ctx.cro_size;
          exports[i].offs_name = name_offset;
-         exports[i].seg_offset = addr;
+         exports[i].seg_offset = addr; //TODO
          
          //TODO: export tree
          //TODO: control offset
@@ -148,29 +159,29 @@ int main(int argc, char **argv)
          //TODO: OnExit
          //TODO: OnUnresolved
          
-         push_data(cro_data, cro_size, name.c_str(), name.length()+1);
+         push_data(cro_ctx, name.c_str(), name.length()+1);
       }
    }
-   export_strtab_size = cro_size - export_strtab_offset;
+   export_strtab_size = cro_ctx.cro_size - export_strtab_offset;
    
-   cro_header->offs_export_strtab = export_strtab_offset;
-   cro_header->size_export_strtab = export_strtab_size;
-   alloc_align_up(cro_data, cro_size, 0x4);
+   cro_ctx.cro_header->offs_export_strtab = export_strtab_offset;
+   cro_ctx.cro_header->size_export_strtab = export_strtab_size;
+   cro_align_up(cro_ctx, 0x4);
    
    // Push imports
    
    // Push data
-   segment_start[SEG_DATA] = cro_size;
-   /*push_segment(cro_data, cro_size, elf.segments[SEG_DATA]);
-   alloc_align_up(cro_data, cro_size, 0x1000, 0xCC);*/
-   segment_size[SEG_DATA] = cro_size - segment_start[SEG_DATA];
+   segment_start[SEG_DATA] = cro_ctx.cro_size;
+   /*push_segment(cro_ctx, elf.segments[SEG_DATA]);
+   alloc_align_up(cro_ctx, 0x1000, 0xCC);*/
+   segment_size[SEG_DATA] = cro_ctx.cro_size - segment_start[SEG_DATA];
    
    // Finalize
-   cro_header->offs_text = segment_start[SEG_TEXT];
-   cro_header->size_text = segment_size[SEG_TEXT] + segment_size[SEG_RODATA];
-   cro_header->offs_data = segment_start[SEG_DATA];
-   cro_header->size_data = segment_size[SEG_DATA];
-   cro_header->size_file = cro_size;
+   cro_ctx.cro_header->offs_text = segment_start[SEG_TEXT];
+   cro_ctx.cro_header->size_text = segment_size[SEG_TEXT] + segment_size[SEG_RODATA];
+   cro_ctx.cro_header->offs_data = segment_start[SEG_DATA];
+   cro_ctx.cro_header->size_data = segment_size[SEG_DATA];
+   cro_ctx.cro_header->size_file = cro_ctx.cro_size;
    
    FILE* cro_file = fopen(argv[2], "wb");
    if (!cro_file)
@@ -179,6 +190,8 @@ int main(int argc, char **argv)
       return -1;
    }
 
-   fwrite(cro_header, sizeof(uint8_t), cro_size, cro_file);
+   printf("asdf\n");
+   printf("Writing %zx bytes\n", cro_ctx.cro_size);
+   fwrite(cro_ctx.cro_data, sizeof(uint8_t), cro_ctx.cro_size, cro_file);
    fclose(cro_file);
 }
