e77b5def5e8feafc96d8b1ac99a6bf9ac9639304
shinyquagsire23 <mtinc2@gmail.com>
2022-09-27 15:13:12 -0700

    Import offset export symbols from other CROs in cro_list.txt

--- cro2elf/cro2elf.cpp
+++ cro2elf/cro2elf.cpp
@@ -1,4 +1,6 @@
 #include <cstdlib>
+#include <fstream>
+#include <unordered_map>
 
 #include "elfio/elfio.hpp"
 #include "elfio/elfio_dump.hpp"
@@ -28,7 +30,7 @@ int main(int argc, char **argv)
 {
    if (argc < 3)
    {
-      printf("Usage: %s <input.cro> <output.elf> [code.bin]\n", argv[0]);
+      printf("Usage: %s <input.cro> <output.elf> [cro_list.txt] [code.bin]\n", argv[0]);
       return -1;
    }
    
@@ -43,12 +45,12 @@ int main(int argc, char **argv)
    FILE* static_file;
    void* static_data = nullptr;
    uint32_t static_size = 0;
-   if (argc > 3)
+   if (argc > 4)
    {
-      static_file = fopen(argv[3], "rb");
+      static_file = fopen(argv[4], "rb");
       if (!static_file)
       {
-         printf("Failed to open file %s! Exiting...\n", argv[2]);
+         printf("Failed to open file %s! Exiting...\n", argv[4]);
          return -1;
       }
       
@@ -302,27 +304,27 @@ int main(int argc, char **argv)
       snprintf(name, 256, "import_index_%s_%u", module->get_name(cro_data), symbol->offs_name);
       int index = symd.add_symbol(stra, name, 0x0, 0, STB_GLOBAL, STT_NOTYPE, 0, 0);
       
-      if (!patch_offs) continue;
-      
-      CRO_Relocation* reloc = (CRO_Relocation*)((char*)cro_data + patch_offs);
-      while(1)
-      {
-         int rel_seg_idx = reloc->seg_offset & 0xf;
-         int rel_seg_offs = reloc->seg_offset >> 4;
-         
-         printf("rel index %x %x %x %x\n", reloc->seg_offset, reloc->type, reloc->addend, reloc->last_entry);
-         
-         if (last_rela != rel_seg_idx)
+      if (patch_offs) {
+         CRO_Relocation* reloc = (CRO_Relocation*)((char*)cro_data + patch_offs);
+         while(1)
          {
-            if (rel_accessor != nullptr)
-               delete rel_accessor;
-            rel_accessor = new relocation_section_accessor(elf, add_relocation_section(elf, sections, dynsym_sec, rel_seg_idx));
+            int rel_seg_idx = reloc->seg_offset & 0xf;
+            int rel_seg_offs = reloc->seg_offset >> 4;
+            
+            //printf("rel index %x %x %x %x\n", reloc->seg_offset, reloc->type, reloc->addend, reloc->last_entry);
+            
+            if (last_rela != rel_seg_idx)
+            {
+               if (rel_accessor != nullptr)
+                  delete rel_accessor;
+               rel_accessor = new relocation_section_accessor(elf, add_relocation_section(elf, sections, dynsym_sec, rel_seg_idx));
+            }
+            rel_accessor->add_entry(segments[rel_seg_idx]->get_virtual_address() + rel_seg_offs, index, reloc->type, reloc->addend);
+            last_rela = rel_seg_idx;
+            
+            if (reloc->last_entry) break;
+            reloc++;
          }
-         rel_accessor->add_entry(segments[rel_seg_idx]->get_virtual_address() + rel_seg_offs, index, reloc->type, reloc->addend);
-         last_rela = rel_seg_idx;
-         
-         if (reloc->last_entry) break;
-         reloc++;
       }
 
       remaining_in_module -= 1;
@@ -347,27 +349,27 @@ int main(int argc, char **argv)
       snprintf(name, 256, "offset_import_%s_%x_%x", module->get_name(cro_data), seg_idx, seg_offs);
       int index = symd.add_symbol(stra, name, 0x0, 0, STB_GLOBAL, STT_NOTYPE, 0, 0);
       
-      if (!patch_offs) continue;
-      
-      CRO_Relocation* reloc = (CRO_Relocation*)((char*)cro_data + patch_offs);
-      while(1)
-      {
-         int rel_seg_idx = reloc->seg_offset & 0xf;
-         int rel_seg_offs = reloc->seg_offset >> 4;
-         
-         //printf("rel offset %x %x %x %x\n", reloc->seg_offset, reloc->type, reloc->addend, reloc->last_entry);
-         
-         if (last_rela != rel_seg_idx)
+      if (patch_offs) {
+         CRO_Relocation* reloc = (CRO_Relocation*)((char*)cro_data + patch_offs);
+         while(1)
          {
-            if (rel_accessor != nullptr)
-               delete rel_accessor;
-            rel_accessor = new relocation_section_accessor(elf, add_relocation_section(elf, sections, dynsym_sec, rel_seg_idx));
+            int rel_seg_idx = reloc->seg_offset & 0xf;
+            int rel_seg_offs = reloc->seg_offset >> 4;
+            
+            //printf("rel offset %x %x %x %x\n", reloc->seg_offset, reloc->type, reloc->addend, reloc->last_entry);
+            
+            if (last_rela != rel_seg_idx)
+            {
+               if (rel_accessor != nullptr)
+                  delete rel_accessor;
+               rel_accessor = new relocation_section_accessor(elf, add_relocation_section(elf, sections, dynsym_sec, rel_seg_idx));
+            }
+            rel_accessor->add_entry(segments[rel_seg_idx]->get_virtual_address() + rel_seg_offs, index, reloc->type, reloc->addend);
+            last_rela = rel_seg_idx;
+            
+            if (reloc->last_entry) break;
+            reloc++;
          }
-         rel_accessor->add_entry(segments[rel_seg_idx]->get_virtual_address() + rel_seg_offs, index, reloc->type, reloc->addend);
-         last_rela = rel_seg_idx;
-         
-         if (reloc->last_entry) break;
-         reloc++;
       }
 
       remaining_in_module -= 1;
@@ -394,6 +396,71 @@ int main(int argc, char **argv)
       }
       rel_accessor->add_entry(segments[rel_seg_idx]->get_virtual_address() + rel_seg_offs, ref_seg_idx+1, reloc->type, segments[ref_seg_idx]->get_virtual_address() + reloc->addend);
       last_rela = rel_seg_idx;
+   }
+
+   // Gather offsets that CROs are interested in
+   std::unordered_map<uint32_t, int> already_added_map;
+   if (argc > 3)
+   {
+      std::ifstream file(argv[3]);
+      if (file.is_open()) {
+         std::string line;
+         while (std::getline(file, line)) {
+            FILE* cro_file_2 = fopen(line.c_str(), "rb");
+            if (!cro_file_2)
+            {
+               printf("Failed to open file %s! Exiting...\n", line.c_str());
+               return -1;
+            }
+
+            fseek(cro_file_2, 0, SEEK_END);
+            uint32_t cro_size_2 = ftell(cro_file_2);
+            void* cro_data_2 = malloc(cro_size_2);
+            
+            fseek(cro_file_2, 0, SEEK_SET);
+            fread(cro_data_2, sizeof(uint8_t), cro_size_2, cro_file_2);
+            fclose(cro_file_2);
+            
+            CRO_Header* cro_header_2 = (CRO_Header*)cro_data_2;
+            printf("Loading info from CRO %s\n", cro_header_2->get_name(cro_data_2));
+
+            if (!strcmp(cro_header->get_name(cro_data), cro_header_2->get_name(cro_data))) {
+               free(cro_data_2);
+               continue;
+            }
+
+            module_count = 0;
+            remaining_in_module = cro_header_2->get_module_entry(cro_data_2, module_count)->import_anonymous_symbol_num;
+            for (int i = 0; i < cro_header_2->num_offset_imports; i++)
+            {
+               CRO_ModuleEntry* module = cro_header_2->get_module_entry(cro_data_2, module_count);
+
+               CRO_Symbol* symbol = cro_header_2->get_offset_import(cro_data_2, i);
+               uint32_t patch_offs = symbol->seg_offset;
+               int seg_idx = symbol->offs_name & 0xf;
+               int seg_offs = symbol->offs_name >> 4;
+
+               uint32_t static_addr = segments[seg_idx]->get_virtual_address() + seg_offs;
+               if (!strcmp(module->get_name(cro_data_2), cro_header->get_name(cro_data)) && already_added_map.find(static_addr) == already_added_map.end()) {
+                  char name[256];
+                  snprintf(name, 256, "offset_import_%s_%x_%x", module->get_name(cro_data_2), seg_idx, seg_offs);
+                  int index = symd.add_symbol(stra, name, static_addr, 0, STB_GLOBAL, STT_NOTYPE, 0, sections[seg_idx]->get_index());
+
+                  already_added_map[static_addr] = 1;
+                  printf("%s\n", name);
+               }
+
+               remaining_in_module -= 1;
+               if (remaining_in_module <= 0) {
+                  module_count++;
+                  remaining_in_module = cro_header_2->get_module_entry(cro_data_2, module_count)->import_anonymous_symbol_num;
+               }
+            }
+
+            free(cro_data_2);
+         }
+         file.close();
+      }
    }
    
    elf.save(argv[2]);
